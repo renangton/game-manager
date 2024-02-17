c5f8c23269fbfcde3207cf3bd1b3ec865b879875
renangton <puccho0427@gmail.com>
2024-02-18 02:48:21 +0900

    first commit

---
+++ src/test/java/com/gamemanager/GameManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
