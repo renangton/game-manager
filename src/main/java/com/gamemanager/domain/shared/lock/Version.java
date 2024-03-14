ae27284b6158c8fb5f868c3378a6f30b2684012b
renangton <puccho0427@gmail.com>
2024-03-15 02:23:41 +0900

    CheckstyleのJavadocのチェックを対象外に変更、Checkstyleの指摘事項に対応

--- src/main/java/com/gamemanager/domain/shared/lock/Version.java
+++ src/main/java/com/gamemanager/domain/shared/lock/Version.java
@@ -10,8 +10,12 @@ public record Version(int value) {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Version version = (Version) o;
         return value() == version.value();
     }
