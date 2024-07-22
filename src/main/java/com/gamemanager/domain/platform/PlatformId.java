2344c540ad1e3acaaa03e906ca9b00a00a9a072f
renangton <puccho0427@gmail.com>
2024-07-23 01:44:44 +0900

    domainにequalsとhashcodeをオーバーライド

--- src/main/java/com/gamemanager/domain/platform/PlatformId.java
+++ src/main/java/com/gamemanager/domain/platform/PlatformId.java
@@ -1,6 +1,7 @@
 package com.gamemanager.domain.platform;
 
 import com.github.f4b6a3.ulid.UlidCreator;
+import java.util.Objects;
 
 public class PlatformId {
     private final String value;
@@ -22,5 +23,18 @@ public class PlatformId {
 
     private boolean isValidUlid(String ulid) {
         return ulid != null && ulid.matches("^[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}$");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlatformId that = (PlatformId) o;
+        return Objects.equals(getValue(), that.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
     }
 }
